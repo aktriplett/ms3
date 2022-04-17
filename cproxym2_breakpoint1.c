fa434d5ca0d003e60e4ab429385c4bf00e471ed9
Amanda Triplett <amanda_triplett@dhcp-10-134-182-82.uawifi.arizona.edu>
2022-04-17 10:57:11 -0700

    next commit

---
+++ cproxym2_breakpoint1.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define BUFFERSIZE 1024
+
+//method called error, exit(0) means you are exiting from the program
+void error(char *msg)
+{
+  perror(msg);
+  exit(0);
+}
+//initialize vars for all functions
+struct sockaddr_in telnet_addr, sproxy_addr;
+char telnetbuf[BUFFERSIZE], sproxybuf[BUFFERSIZE], packetbuf[BUFFERSIZE];
+int option = 1;
+
+//FUNCTIONS
+int TelnetConnect(int portno)
+{
+  // Create socket
+  int TelnetSocket = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(TelnetSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+
+  if (TelnetSocket < 0)
+  {
+    error("ERROR opening Telnet socket");
+  }
+
+  bzero((char *) &telnet_addr, sizeof(telnet_addr));//clearing telnet_addr with bzero method
+  telnet_addr.sin_family = AF_INET;//saying to server address, all will be in internet address concept
+  telnet_addr.sin_addr.s_addr = INADDR_ANY;// get your address on your own when you start the program
+  telnet_addr.sin_port = htons(portno);// convert integer format to network format with htons
+
+  if (bind(TelnetSocket, (struct sockaddr *) &telnet_addr, sizeof(telnet_addr)) < 0)
+  {
+    error("ERROR on binding Telnet Socket");
+  }
+  return TelnetSocket;
+}
+
+int SproxyConnect(char *host, int portno)
+{
+  // Create socket
+  int SproxySocket = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(SproxySocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+
+  if (SproxySocket < 0)
+  {
+    error("ERROR opening Sproxy socket");
+  }
+
+  bzero((char *) &sproxy_addr, sizeof(sproxy_addr));
+  sproxy_addr.sin_family = AF_INET;
+  inet_aton(host, &sproxy_addr.sin_addr.s_addr);
+  sproxy_addr.sin_port = htons(portno);
+
+  fprintf(stderr,"Received and converted server IP\n");
+
+  return SproxySocket;
+}
+
+char* setPacket(int type, char* payload, int len, int seq)
+{
+   bzero(packetbuf, sizeof(packetbuf));
+   char *p = packetbuf; //points to start of packetbuf
+   *((int*) p) = type;//loads raw integer data to start of p pointer
+   p = p + 4; //move pointer by size of int
+   *((int*) p) = seq;
+   p = p + 4;
+   *((int*) p) = len;
+   p = p + 4;
+   memcpy(p, payload, len);
+   return packetbuf;
+}
+
+int getPacketType(char* packet)
+{
+    return *((int*) packet);//starting from initial packet pointer and moving an integer length down is int data
+}
+
+char* getPacketMsg(char* packet)
+{
+    return packet + 12;
+}
+
+int main(int argc, char *argv[])
+{
+    int TelnetSocket, SproxySocket;
+    int telnetport, sproxyport;
+    socklen_t len1;
+    fd_set readfds;
+    struct timeval tv;
+    int rv;
+    int n, len = 0;
+    int hbcount = 0;
+    int telnetrecv, sproxyrecv = 0;
+    char buf1[BUFFERSIZE],buf2[BUFFERSIZE];
+
+    if (argc < 3)//have all necessary command line arguments been given
+    {
+      fprintf(stderr, "usage %s hostname port\n", argv[0]);
+      exit(0);
+    }
+
+    telnetport = atoi(argv[1]);//port no passed in command line arg, to convert character to int we use atoi
+    sproxyport = atoi(argv[3]);
+    TelnetSocket = TelnetConnect(telnetport);//calling socket set up functions
+    SproxySocket = SproxyConnect(argv[2],sproxyport);
+
+    listen(TelnetSocket, 5);
+
+    while(1)
+    {
+      fprintf(stderr,"I'm listening on telnet\n");
+
+      int newtelnetsocket = accept(TelnetSocket, (struct sockaddr *) &telnet_addr, &len1);
+      if (newtelnetsocket < 0)
+      {
+        error("ERROR on telnet accept");
+      }
+      fprintf(stderr,"Connected to telnet local host\n");
+
+      //connect to sproxy
+      if (connect(SproxySocket, &sproxy_addr, sizeof(sproxy_addr)) < 0)
+      {
+        error("ERROR connecting to sproxy");
+      }
+      fprintf(stderr,"Connected to sproxy\n");
+
+      FD_ZERO(&readfds);// clear the set
+      FD_SET(newtelnetsocket, &readfds);// add descriptors (fd) to set
+      FD_SET(SproxySocket, &readfds);
+      if (newtelnetsocket > SproxySocket) n = newtelnetsocket + 1;// find the largest descriptor, and plus one.
+      else n = SproxySocket + 1;
+      tv.tv_sec = 10;//timeout is 10.5 sec to receive data on either socket
+      tv.tv_usec = 0; //this is .5 sec
+
+      //Begin message sending loop
+      while((rv = select(n, &readfds, NULL, NULL, &tv)) >= 0)
+      {
+        setPacket(1, "hb", 2, hbcount);//we know we have to send a heartbeat format message (ID 1)
+        fprintf(stderr, "set the hb packet\n");
+        //send(SproxySocket, packetbuf, 14, 0);//send the heartbeat contained in packet buf to sproxy
+        //fprintf(stderr,"Client sent a heartbeat message to server:%s\n",packetbuf);
+        if (rv == 0)
+        {
+          hbcount++;
+          //heartbeat hits 3 so we assume the connection timed out and we close
+          if (hbcount == 3)
+          {
+            //fprintf(stderr,"hb hit three, reset\n");
+            hbcount = 0;//reset hb count
+            fprintf(stderr, "reset hb\n");
+            //close(SproxySocket);//close disconnected socket
+
+            //int SproxySocket = SproxyConnect(argv[2],sproxyport);
+
+            //if (connect(SproxySocket, &sproxy_addr, sizeof(sproxy_addr)) < 0)
+            //{
+            //  error("ERROR connecting NEW sproxy\n");
+            //}
+            //fprintf(stderr,"cproxy made a NEW connection to sproxy\n");
+          }
+        }
+
+        else
+        {
+          bzero(buf1, sizeof(buf1));
+          bzero(buf2, sizeof(buf2));
+          // one or both of the descriptors have data
+          if (FD_ISSET(newtelnetsocket, &readfds))
+          {
+              len = recv(newtelnetsocket, buf1, sizeof(buf1), 0);
+              if (len < 0)
+              {
+                error("ERROR on telnet receive\n");
+                break;
+              }
+              fprintf(stderr, "Received message from telnet\n");
+              send(SproxySocket, buf1, len, 0);
+          }
+
+          if (FD_ISSET(SproxySocket, &readfds))
+          {
+              len = recv(SproxySocket, buf2, sizeof(buf2), 0);
+              if (len < 0)
+              {
+                error("ERROR on sproxy receive\n");
+                break;
+              }
+              fprintf(stderr, "Received message from sproxy\n");
+              send(newtelnetsocket, buf2, len, 0);
+          }
+        }
+        FD_ZERO(&readfds);// clear the set
+        FD_SET(newtelnetsocket, &readfds);// add descriptors (fd) to set
+        FD_SET(SproxySocket, &readfds);
+        if (newtelnetsocket > SproxySocket) n = newtelnetsocket + 1;// find the largest descriptor, and plus one.
+        else n = SproxySocket + 1;
+        tv.tv_sec = 10;//timeout is 10.5 sec to receive data on either socket
+        tv.tv_usec = 0; //this is .5 sec
+      }
+      close(SproxySocket,2);
+      close(newtelnetsocket,2);
+      break;
+  }
+  return 0;
+}
---
+++ sproxym2_breakpoint1.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define BUFFERSIZE 1024
+
+//method called error, exit(1) means you are exiting from the program
+void error(char *msg)
+{
+  perror(msg);
+  exit(1);
+}
+
+//initialize vars for all functions
+struct sockaddr_in daemon_addr, cproxy_addr;
+char *daemonip = "127.0.0.1";
+char daemonbuf[BUFFERSIZE], cproxybuf[BUFFERSIZE], packetbuf[BUFFERSIZE];
+int option = 1;
+
+int DaemonConnect()
+{
+  // Create socket.
+  int DaemonSocket = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(DaemonSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+  if (DaemonSocket < 0)
+  {
+    error("ERROR opening Daemon socket");
+  }
+
+  bzero((char *) &daemon_addr, sizeof(daemon_addr));//clearing _addr with bzero method
+  daemon_addr.sin_family = AF_INET;//saying to server address, all will be in internet address concept
+  inet_aton(daemonip, &daemon_addr.sin_addr.s_addr);// converting char ip addr
+  daemon_addr.sin_port = htons(23);// convert integer format to network format with htons
+  fprintf(stderr,"Received and converted Daemon IP\n");
+
+  return DaemonSocket;
+}
+
+int CproxyConnect(int portno)
+{
+  int CproxySocket = socket(AF_INET, SOCK_STREAM, 0);// Create socket.
+  setsockopt(CproxySocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+  if (CproxySocket < 0)
+  {
+    error("ERROR opening Cproxy socket");
+  }
+
+  bzero((char *) &cproxy_addr, sizeof(cproxy_addr));
+  cproxy_addr.sin_family = AF_INET;
+  cproxy_addr.sin_addr.s_addr = INADDR_ANY;
+  cproxy_addr.sin_port = htons(portno);
+
+  if (bind(CproxySocket, (struct sockaddr *) &cproxy_addr, sizeof(cproxy_addr)) < 0)
+  {
+    error("ERROR on binding Cproxy Socket");
+  }
+
+  return CproxySocket;
+}
+
+char* setPacket(int type, char* payload, int len, int seq)
+{
+   bzero(packetbuf, sizeof(packetbuf));
+   char *p = packetbuf;
+   *((int*) p) = type;
+   p = p + 4;
+   *((int*) p) = seq;
+   p = p + 4;
+   *((int*) p) = len;
+   p = p + 4;
+   memcpy(p, payload, len);
+   return packetbuf;
+}
+
+int getPacketType(char* packet)
+{
+    return *((int*) packet);
+}
+
+char* getPacketMsg(char* packet)
+{
+    return packet + 12;
+}
+
+int main(int argc, char *argv[])
+{
+  int DaemonSocket, CproxySocket;
+  int cproxyport;
+  socklen_t len1;
+  fd_set readfds;
+  struct timeval tv;
+  int rv;
+  int n, len = 0;
+  int cproxyrecv, daemonrecv = 0;
+  int seqNum = 0;
+  int hbcount = 0;
+  char buf1[BUFFERSIZE],buf2[BUFFERSIZE];
+
+  if (argc < 1)// have all necessary command line arguments been given
+  {
+    fprintf(stderr, "ERROR, no port provided\n");
+    exit(1);
+  }
+
+  cproxyport = atoi(argv[1]);//port no passed in command line arg, to convert character to int we use atoi
+  DaemonSocket = DaemonConnect();//calling socket set up functions
+  CproxySocket = CproxyConnect(cproxyport);
+
+  listen(CproxySocket, 5);//going into listen mode on sproxy, can handle 5 clients
+
+  while(1)
+  {
+    fprintf(stderr,"I'm listening on cproxy socket\n");
+    int newcproxysocket = accept(CproxySocket, (struct sockaddr *) &cproxy_addr, &len1);
+    if (newcproxysocket < 0)
+    {
+      error("ERROR on cproxy accept");
+    }
+    fprintf(stderr,"Connected to a client on cproxy\n");
+
+    //connect to telnet daemon
+    if (connect(DaemonSocket, &daemon_addr, sizeof(daemon_addr)) < 0)
+    {
+      error("ERROR connecting to daemon");
+    }
+    fprintf(stderr,"Connected to telnet daemon\n");
+
+    FD_ZERO(&readfds);// clear the set ahead of time
+    FD_SET(newcproxysocket, &readfds);// add our descriptors to the set
+    FD_SET(DaemonSocket, &readfds);
+    if (newcproxysocket > DaemonSocket) n = newcproxysocket + 1;// find the largest descriptor, and plus one.
+    else n = DaemonSocket + 1;
+    tv.tv_sec = 10;// timeout is 10.5 sec to receive data on either socket
+    tv.tv_usec = 0; //this is .5 sec
+
+    //Entering the message loop
+    while((rv = select(n, &readfds, NULL, NULL, &tv)) >= 0)
+    {
+      setPacket(1, "hb", 2, hbcount);//we know we have to send a heartbeat format message
+      fprintf(stderr, "set the hb packet\n");
+      //send(newcproxysocket, packetbuf, sizeof(packetbuf), 0);//send the heartbeat
+
+      if (rv == 0)
+      {
+        hbcount++;
+        if (hbcount == 3)
+        {
+            fprintf(stderr, "reset hb\n");
+            hbcount = 0;
+            //fprintf(stderr,"hb hit three, reset\n");
+            // close(newcproxysocket);
+            // int newcproxysocket = accept(CproxySocket, (struct sockaddr *) &cproxy_addr, &len1);
+            //
+            // if (newcproxysocket < 0)
+            // {
+            //   error("ERROR on NEW cproxy accept");
+            // }
+            //
+            // FD_SET(newcproxysocket, &readfds);
+            // fprintf(stderr,"sproxy reconnected to cproxy\n");
+         }
+      }
+      else
+      {
+         bzero(buf1, sizeof(buf1));
+         bzero(buf2, sizeof(buf2));
+         // one or both of the descriptors have data
+         if (FD_ISSET(newcproxysocket, &readfds))
+         {
+             len = recv(newcproxysocket, buf1, sizeof(buf1), 0);
+             if (len < 0)
+             {
+               error("ERROR on cproxy receive\n");
+               break;
+             }
+             fprintf(stderr, "Received message from cproxy\n");
+             send(DaemonSocket, buf1, len, 0);
+         }
+         if (FD_ISSET(DaemonSocket, &readfds))
+         {
+             len = recv(DaemonSocket, buf2, sizeof(buf2), 0);
+             if (len < 0)
+             {
+               error("ERROR on daemon receive\n");
+               break;
+             }
+             fprintf(stderr, "Received message from daemon\n");
+             send(newcproxysocket, buf2, len, 0);
+         }
+       }
+       FD_ZERO(&readfds);// clear the set ahead of time
+       FD_SET(newcproxysocket, &readfds);// add our descriptors to the set
+       FD_SET(DaemonSocket, &readfds);
+       if (newcproxysocket > DaemonSocket) n = newcproxysocket + 1;// find the largest descriptor, and plus one.
+       else n = DaemonSocket + 1;
+       tv.tv_sec = 10;// timeout is 10.5 sec to receive data on either socket
+       tv.tv_usec = 0; //this is .5 sec
+     }
+     close(DaemonSocket,2);
+     close(newcproxysocket,2);
+  }
+  return 0;
+}
