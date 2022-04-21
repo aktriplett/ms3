02192b5bdcbbeaea67ea7d1cff3815962a72065f
Amanda Triplett <amanda_triplett@Amandas-iMac.local>
2022-04-20 21:13:47 -0700

    next commit

--- cproxy.c
+++ cproxy.c
@@ -179,8 +179,7 @@ int main(int argc, char *argv[])
             hbcount = 0;//reset hb count
             close(SproxySocket);//close disconnected socket
             int SproxySocket = SproxyConnect(argv[2],sproxyport);
-            int check = 2;
-            check = connect(SproxySocket, &sproxy_addr, sizeof(sproxy_addr));
+            int check = connect(SproxySocket, &sproxy_addr, sizeof(sproxy_addr));
             if (check < 0)
             {
               error("ERROR connecting NEW sproxy\n");
@@ -194,12 +193,9 @@ int main(int argc, char *argv[])
 
             else
             {
-              if(bind(TelnetSocket, (struct sockaddr *) &telnet_addr, sizeof(telnet_addr)) < 0)
-              {
-                fprintf(stderr,"ERROR on binding Telnet test\n");
                 break;
-              }
             }
+          }
 
           }
           tv.tv_sec = 1;
